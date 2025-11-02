68b7abd19fe9e3e161d8b4c0912cead2a02842ba
aliceteles-dev <amteles@outlook.com>
2025-11-02 15:14:56 -0300

    chlg4

--- Desafios/cap7_desafio4.py
+++ Desafios/cap7_desafio4.py
@@ -1,3 +1,5 @@
+#escreva um programa com um loop infinito (com a opção de digitar q para sair) e uma lista de números. A cada iteração do loop, peça ao usuário para fornecer um
+#número da lista e informe se o seu palpite estava ou não correto.
 lista = [1, 3, 5, 7, 9, 11]
 
 while True:
