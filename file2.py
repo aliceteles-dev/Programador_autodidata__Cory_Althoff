0e3a084113918f10b675a1a93109b53659acd4b3
aliceteles-dev <amteles@outlook.com>
2025-11-05 12:56:09 -0300

    leitura do capítulo 9 - arquivos

---
+++ file2.py
@@ -0,0 +1,4 @@
+with open("st2.txt", "w") as f:
+    f.write("Once again, hi from Python!")
+
+#este código abre o arquivo, executa os comandos e já fecha o arquivo sem que eu precise me preocupar com close()
